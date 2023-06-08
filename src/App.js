f5735806ca3b2028d57591f0f603c8c4e780fff5
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-06-08 09:33:45 +0300

    done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/navigation/Navbar';
+//import Pets from './components/Pets';
+
+import "../src/style.css";
+
+
+export default function App() {
+  return (
+    <>
+      <Navbar/>
+      {/* <Pets /> */}
+    </>
+  );
+}
